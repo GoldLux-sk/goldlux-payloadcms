6aefb0e2ee179c3e4f126683e0d108dfc932025f
AdamDemian <adamdemian1@gmail.com>
2023-04-06 15:02:06 +0200

    cors error

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -2,16 +2,17 @@ import { buildConfig } from 'payload/config';
 import path from 'path';
 // import Examples from './collections/Examples';
 import Users from './collections/Users';
+import Orders from './collections/Orders';
 
 export default buildConfig({
   serverURL: 'http://localhost:3000',
+  cors: ("*"),
   admin: {
     user: Users.slug,
   },
   collections: [
     Users,
-    // Add Collections here
-    // Examples,
+    Orders,
   ],
   typescript: {
     outputFile: path.resolve(__dirname, 'payload-types.ts'),
