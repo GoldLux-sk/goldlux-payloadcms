951646596405b2d64b6e0734f1ee2ccab11cea77
AdamDemian <adamdemian1@gmail.com>
2023-06-18 09:31:43 +0200

    payload ready for dev fe

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -6,8 +6,16 @@ import Orders from './collections/Orders';
 
 export default buildConfig({
   serverURL: process.env.PAYLOAD_PUBLIC_BASE_DNS || 'http://localhost:3000',
-  cors: ['http://localhost:3000', 'https://adamdemian1-gmailcom-goldlux-payloadcms.payloadcms.app'],
-  csrf: ['https://adamdemian1-gmailcom-goldlux-payloadcms.payloadcms.app', 'http://localhost:3000'],
+  cors: [
+    'http://localhost:3000',
+    'https://adamdemian1-gmailcom-goldlux-payloadcms.payloadcms.app',
+    'https://goldlux-nextjs.vercel.app'
+  ],
+  csrf: [
+    'https://adamdemian1-gmailcom-goldlux-payloadcms.payloadcms.app',
+    'http://localhost:3000',
+    'https://goldlux-nextjs.vercel.app'
+  ],
   admin: {
     user: Users.slug,
   },
