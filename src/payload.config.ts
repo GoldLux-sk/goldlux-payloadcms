2529543534a21c7795189c021e3726b8ed729fc0
AdamDemian <adamdemian1@gmail.com>
2023-07-05 22:29:54 +0200

    domains add

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -8,16 +8,17 @@ export default buildConfig({
   cors: [
     'http://localhost:3000',
     'http://localhost:3001',
+    'https://beautiful-brigadeiros-64c3aa.netlify.app',
+    'https://goldlux-payloadcms.payloadcms.app',
     process.env.NEXT_PUBLIC_BASE_DNS || 'http://localhost:3001',
-    'https://adamdemian1-gmailcom-goldlux-payloadcms.payloadcms.app',
-    'https://goldlux-nextjs.vercel.app',
+
   ],
   csrf: [
-    'https://adamdemian1-gmailcom-goldlux-payloadcms.payloadcms.app',
     'http://localhost:3000',
     'http://localhost:3001',
+    'https://beautiful-brigadeiros-64c3aa.netlify.app',
+    'https://goldlux-payloadcms.payloadcms.app',
     process.env.NEXT_PUBLIC_BASE_DNS || 'http://localhost:3001',
-    'https://goldlux-nextjs.vercel.app'
   ],
   admin: {
     user: Users.slug,
