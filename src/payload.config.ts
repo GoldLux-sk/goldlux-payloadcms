4281487f653dd0d03027f6c601e17ddc18a978a8
AdamDemian <adamdemian1@gmail.com>
2023-07-23 18:06:04 +0200

    custom styles

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -2,6 +2,8 @@ import { buildConfig } from 'payload/config';
 import path from 'path';
 import Users from './collections/Users';
 import Orders from './collections/Orders';
+import Icon from './graphics/Icon';
+import Logo from './graphics/Logo';
 import dotenv from 'dotenv';
 
 dotenv.config({
@@ -18,6 +20,18 @@ export default buildConfig({
   ].filter(Boolean),
   admin: {
     user: Users.slug,
+    css: path.resolve(__dirname, './admin.scss'),
+    meta: {
+      titleSuffix: ' - Goldlux',
+      favicon: '/assets/logo.svg',
+      ogImage: '/assets/logo.svg',
+    },
+    components: {
+      graphics: {
+        Icon,
+        Logo
+      }
+    }
   },
   collections: [
     Users,
